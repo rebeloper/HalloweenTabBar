34d5c30f23615df7e72f6b63eb011dd4f78eeaff
Alex <therebeloper@gmail.com>
2018-10-26 16:25:04 +0300

    Initial Commit

---
+++ HalloweenTabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HalloweenTabBar
+//
+//  Created by Alex Nagy on 26/10/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
